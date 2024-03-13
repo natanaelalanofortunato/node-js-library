a23b50b36e7d0e4bee1111a6edb5922023f8e3f2
natanaelalanofortunato <natanael.a.fortunato91@gmail.com>
2024-03-12 22:34:25 -0300

    add: adding some new code to index.js

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.blue('Hello world!'));
+function errorDeal (erro) {
+    console.log(chalk.blue(erro));
+    throw new Error(chalk.red(erro.code, 'Theres no file in this directory.'));
+}
+
+/* Async - Await */
+async function getFile(filePath) {
+    try {
+        const encode = 'utf-8';
+        const text = await fs.promises
+        .readFile(filePath, encode)
+        extractLinks(text);
+    }   catch (erro) {
+        errorDeal(erro);
+    }
+}
+
+/* Some times chalk cant return the correct value while using on some cases */
+
+function extractLinks (value) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captures = [...value.matchAll(regex)];
+    const results = captures.map(capture => ({[capture[1]]: capture[2]}));
+    console.log(results);
+}
+
+/* Promises with then
+function getFile(filePath) {
+    const encode = 'utf-8';
+    fs.promises
+    .readFile(filePath, encode)
+    .then((text) => console.log(chalk.green(text)))
+    .catch((erro) => errorDeal(erro))
+}
+*/
+
+getFile('./arquivos/texto.md');
